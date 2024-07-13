d73a1591c3381e495d7ac6f260db594e8e788b18
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-13 16:41:25 +0530

    feat: almost complete hero section

--- src/components/organisms/HeroSection/index.tsx
+++ src/components/organisms/HeroSection/index.tsx
@@ -11,8 +11,13 @@ const HeroSection = () => {
         <Heading1 text='Nahyan Sharvin' />
         <p className='font-light leading-normal text-xl max-w-md pl-7 mt-20 border-l border-foreground/50 opacity-60'>I'm a self taught software engineer and designer based in Kerala, India. I love building applications and solving problems.</p>
       </div>
-      <div>
-        <Image src='/images/hero-image.png' width={1000} height={500} alt='Hero Image' priority />
+      <div className='relative'>
+        {/* Download CV button */}
+        <a href='#' className='absolute top-48 left-9 bg-secondary text-white font-display font-medium text-lg px-5 p-1.5 rounded-full'>
+          <Image src='/images/download-icon.svg' width={28} height={28} alt='Download Icon' />
+          CV
+        </a>
+        <Image src='/images/hero-image-full.png' width={1000} height={500} alt='Hero Image' className='mt-5' priority />
       </div>
     </section>
   )
