694f9dde04b496c0e191fc20e3bc6ee0c8a5c2f7
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-13 23:05:00 +0530

    fix: change skill image size

--- src/components/organisms/SkillsSection/index.tsx
+++ src/components/organisms/SkillsSection/index.tsx
@@ -6,7 +6,7 @@ const SkillsSection = () => {
   return (
     <section id='skills' className='w-full p-section-padding py-14 gap-5'>
         <Heading1>Tech</Heading1>
-        <Image src='/images/tech-stacks.svg' alt='Terch Stacks' width={1100} height={500} className='my-16'/>
+        <Image src='/images/tech-stacks.svg' alt='Terch Stacks' width={1050} height={500} className='my-16'/>
     </section>
   )
 }
