d0f2af5daa50173c4507dc6046e6f7982e2c3b9e
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-13 17:19:09 +0530

    enhan: change heading prop type

--- src/components/atoms/Heading/Heading1.tsx
+++ src/components/atoms/Heading/Heading1.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 type Heading1Props = {
-    text: string
+    children: React.ReactNode
     className?: string
 }
 
 const Heading1: React.FC<Heading1Props> = (props) => {
-  return <h1 className={`text-9xl font-display font-medium ${props.className}`}>{props.text}</h1> 
+  return <h1 className={`text-9xl font-display font-medium ${props.className}`}>{props.children}</h1> 
 }
 
 export default Heading1
