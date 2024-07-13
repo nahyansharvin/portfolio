a76af95e298f97169e4204072066842cc56974bf
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-13 15:06:49 +0530

    feat: start adding navbar component

---
+++ src/components/molecules/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const navList = [
+    {
+        name: 'Home',
+        url: '#hero-section'
+    },
+    {
+        name: 'Projects',
+        url: '#projects-section'
+    },
+    {
+        name: 'Contact',
+        url: '#contact-section'
+    }
+]
+
+const Navbar = () => {
+  return (
+    <div className='absolute top-0 left-0 w-full flex justify-center'>
+        <nav className='w-11/12 h-12 mt-8 p-2 flex items-center justify-between rounded-full bg-background border border-highlight/50'>
+            <h3 id='logo' className='ml-5 font-display font-medium text-xl'>Nahyan Sharvin</h3>
+
+            <ul id='nav-links' className='flex gap-8'>
+                {navList.map((navItem, index) => (
+                    <li key={index} className=''>
+                        <a href={navItem.url}
+                            className='text-foreground'>{navItem.name}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div>
+                <button className='ml-auto mr-5 text-foreground'>Hire Me</button>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
         'background': 'rgba(var(--background-rgb))',
         'secondary': 'rgba(var(--secondary-rgb))',
         'tertiary': 'rgba(var(--tertiary-rgb))',
+        'highlight': 'rgba(var(--highlight-rgb))',
       }
     },
   },
