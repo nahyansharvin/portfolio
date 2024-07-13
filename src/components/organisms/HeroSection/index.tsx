3cea79512b836312fd6629db926b58b573559fc1
Nahyan Sharvin <nskonnalath@gmail.com>
2024-07-13 17:19:34 +0530

    fix: update heading change

--- src/components/organisms/HeroSection/index.tsx
+++ src/components/organisms/HeroSection/index.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <section id='hero-section' className='bg-hero-gradient bg-right-top bg-no-repeat p-section-padding py-44 h-screen overflow-hidden flex'>
+    <section id='hero' className='bg-hero-gradient bg-right-top bg-no-repeat p-section-padding py-44 h-screen overflow-hidden flex'>
       <div>
         <Heading2 text='Software Developer' className='uppercase opacity-60 tracking-wider'/>
-        <Heading1 text='Nahyan Sharvin' />
+        <Heading1>Nahyan Sharvin</Heading1>
         <p className='font-light leading-normal text-xl max-w-md pl-7 mt-20 border-l border-foreground/50 opacity-60'>I'm a self taught software engineer and designer based in Kerala, India. I love building applications and solving problems.</p>
       </div>
       <div className='relative'>
